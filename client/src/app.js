02301a735ddd48ce1bf7bcc997f74c7c0fdf790f
Walker Lindley <walker@walkerlindley.com>
2017-01-14 17:40:13 -0800

    Making client connect to socket.io server.

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,21 @@
-export class App {
-  constructor() {
-    this.message = 'Hello World!';
-  }
+import io from 'socket.io-client';
+let socket = io.connect('http://localhost:80');
 
-  get isLoading() {
-    return false;
-  }
+export class App {
+	constructor() {
+		this.message = 'Connecting...';
+	}
+
+	activate() {
+		socket.on('message', (msg) => {
+			this.message = msg;
+			setTimeout(() => {
+				socket.emit('message', 'client connected');
+			}, 2000);
+		});
+	}
+
+	get isLoading() {
+		return false;
+	}
 }
