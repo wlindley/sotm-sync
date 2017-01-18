ee4918ccaae95d0cb1507956073b7417cac1e191
Walker Lindley <walker@walkerlindley.com>
2017-01-17 20:44:20 -0800

    Starting to establish communication protocol between client and server.

--- client/src/game.js
+++ client/src/game.js
@@ -1,14 +1,22 @@
 import {Api} from './api';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import * as messages from './messages';
 
 export class Game {
-	static inject() { return [Api]; }
+	static inject() { return [Api, EventAggregator]; }
 
-	constructor(api) {
+	constructor(api, ea) {
 		this.api = api;
+		this.ea = ea;
+		this.ea.subscribe(messages.StateSync, msg => this._onStateSync(msg.state));
 	}
 
 	activate(params, routeConfig) {
 		this.gameId = params.id;
 		this.api.joinGame(this.gameId);
+	}
+
+	_onStateSync(state) {
+		console.log('state:', state);
 	}
 }
---
+++ client/src/messages.js
@@ -0,0 +1,5 @@
+export class StateSync {
+	constructor(state) {
+		this.state = state;
+	}
+}
