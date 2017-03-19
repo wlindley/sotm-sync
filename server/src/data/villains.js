3603de5c4933d8122666e129540aae7ee0559086
Walker Lindley <walker@walkerlindley.com>
2017-03-18 17:01:03 -0700

    Added some villain data.

--- server/src/data/villains.js
+++ server/src/data/villains.js
@@ -1,11 +1,308 @@
 module.exports = [
+	/* ===== Akash"Bhuta ===== */
+	{
+		name: "akashbhuta",
+		type: "villain",
+		subtype: "character",
+		inList: true,
+		displayName: "Akash'Bhuta",
+		initialHp: 200,
+		childTargets: ["arborealphalanges", "ensnaringbrambles", "livingrockslide", "mountainouscarapace"]
+	},
+	{
+		name: "arborealphalanges",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Arboreal Phalanges",
+		initialHp: 12
+	},
+	{
+		name: "ensnaringbrambles",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Ensnaring Brambles",
+		initialHp: 9
+	},
+	{
+		name: "livingrockslide",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Living Rockslide",
+		initialHp: 10
+	},
+	{
+		name: "mountainouscarapace",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Mountainous Carapace",
+		initialHp: 11
+	},
+	/* ===== Ambuscade ===== */
+	{
+		name: "ambuscade",
+		type: "villain",
+		subtype: "character",
+		inList: true,
+		displayName: "Ambuscade",
+		initialHp: 50,
+		childTargets: ["automatedturret", "handcannon", "explosivelauncher", "cloakingdevice", "reactiveplating", "sonicmine"]
+	},
+	{
+		name: "automatedturret",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Automated Turret",
+		initialHp: 8
+	},
+	{
+		name: "handcannon",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Custom Hand Cannon",
+		initialHp: 5
+	},
+	{
+		name: "explosivelauncher",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Explosive Launcher",
+		initialHp: 6
+	},
+	{
+		name: "cloakingdevice",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Personal Cloaking Device",
+		initialHp: 9
+	},
+	{
+		name: "reactiveplating",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Reactive Plating",
+		initialHp: 10
+	},
+	{
+		name: "sonicmine",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Sonic Mine",
+		initialHp: 1
+	},
+	/* ===== Apostate ===== */
+	{
+		name: "apostate",
+		type: "villain",
+		subtype: "character",
+		inList: true,
+		displayName: "Apostate",
+		initialHp: 66,
+		childTargets: ["condemnation", "corruptedeffigy", "fiendishpugilist", "gauntletofperdition", "imppilferer", "orbofdelirium", "periaptofwoe", "relicspirit", "runesofmalediction", "tomeoftheunknowable"]
+	},
+	{
+		name: "condemnation",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Condemnation",
+		initialHp: 11
+	},
+	{
+		name: "corruptedeffigy",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Corrupted Effigy",
+		initialHp: 6
+	},
+	{
+		name: "fiendishpugilist",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Fiendish Pugilist",
+		initialHp: 6
+	},
+	{
+		name: "gauntletofperdition",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Gauntlet of Perdition",
+		initialHp: 5
+	},
+	{
+		name: "imppilferer",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Imp Pilferer",
+		initialHp: 3
+	},
+	{
+		name: "orbofdelirium",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Orb of Delirium",
+		initialHp: 12
+	},
+	{
+		name: "periaptofwoe",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Periapt of Woe",
+		initialHp: 4
+	},
+	{
+		name: "relicspirit",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Relic Spirit",
+		initialHp: 9
+	},
+	{
+		name: "runesofmalediction",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Runes of Malediction",
+		initialHp: 6
+	},
+	{
+		name: "tomeoftheunknowable",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Tome of the Unknowable",
+		initialHp: 3
+	},
+	/* ===== Baron Blade ===== */
 	{
 		name: "baronblade",
 		type: "villain",
 		subtype: "character",
 		inList: true,
 		displayName: "Baron Blade",
-		initialHp: 50,
-		childTargets: []
-	}
+		initialHp: 40,
+		childTargets: ["bladebattalion", "elementalredistributor", "defenseplatform", "remoteturret"]
+	},
+	{
+		name: "bladebattalion",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Blade Battalion",
+		initialHp: 5
+	},
+	{
+		name: "elementalredistributor",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Elemental Redistributor",
+		initialHp: 10
+	},
+	{
+		name: "defenseplatform",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Mobile Defense Platform",
+		initialHp: 10
+	},
+	{
+		name: "remoteturret",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Powered Remote Turret",
+		initialHp: 7
+	},
+	/* ===== La Capitan ===== */
+	{
+		name: "lacapitan",
+		type: "villain",
+		subtype: "character",
+		inList: true,
+		displayName: "La Capitan",
+		initialHp: 75,
+		childTargets: ["mable", "battleforged", "chip", "lepeiste", "finalbreath", "paradojamagnifica", "siegebreaker", "trueshot"]
+	},
+	{
+		name: "mable",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "The Amazing Mable",
+		initialHp: 6
+	},
+	{
+		name: "battleforged",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Battle Forged",
+		initialHp: 12
+	},
+	{
+		name: "chip",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Chip",
+		initialHp: 8
+	},
+	{
+		name: "lepeiste",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "L' Epeiste",
+		initialHp: 7
+	},
+	{
+		name: "finalbreath",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Final Breath",
+		initialHp: 11
+	},
+	{
+		name: "paradojamagnifica",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "La Paradoja Magnifica",
+		initialHp: 15
+	},
+	{
+		name: "siegebreaker",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Siege-Breaker",
+		initialHp: 10
+	},
+	{
+		name: "trueshot",
+		type: "villain",
+		subtype: "target",
+		inList: true,
+		displayName: "Trueshot",
+		initialHp: 9
+	},
 ];
