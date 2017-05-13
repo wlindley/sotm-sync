73aff8d30c94f1b7be033dda78a73756697f7f1a
Walker Lindley <walker@walkerlindley.com>
2017-05-13 13:15:52 -0700

    Using rsync with exclude rather than copying files and then deleting some of them.

--- predeploy.sh
+++ predeploy.sh
@@ -2,13 +2,11 @@
 source ./buildlib.sh
 
 mkdir -p deploy/client
-cp -r server/* deploy/
+rsync -av --exclude='spec' --exclude='node_modules' --exclude='coverage' --exclude='npm-debug.log' server/ deploy/
 cp client/index.html deploy/client/
 cp client/favicon.ico deploy/client/
 cp -r client/scripts deploy/client/scripts
 cp -r client/bootstrap deploy/client/bootstrap
-rm -rf deploy/node_modules
-rm -rf deploy/spec
 pushd deploy
 zip -r ../artifact.zip *
 popd
